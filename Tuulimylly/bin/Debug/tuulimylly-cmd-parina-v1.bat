0170f96a220218208d3b015c36bf5546dd3df986
Vaingald <vaino.palonen@icloud.com>
2019-02-12 21:08:02 +0200

    Poistettu lisää turhia tiedostoja.

--- Tuulimylly/bin/Debug/tuulimylly-cmd-parina-v1.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-:kaka
-color 0a
-echo  %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
-goto kaka
--- Tuulimylly/bin/Debug/tuulimylly-cmd-parina-v2.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:kaka
-color 0a
-echo  %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
-color a0
-goto kaka
